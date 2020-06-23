4b10f2440d47553e853b37b1ea8b76ea5fbadf12
filipe.soares <filipecarlossoares@gmail.com>
2020-06-23 12:27:34 -0300

    disconnect mongodb after MongoHelper tests

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -26,7 +26,6 @@ module.exports = class LoginRouter {
       }
       return HttpResponse.ok({ accessToken })
     } catch (error) {
-      console.error(error)
       return HttpResponse.serverError()
     }
   }
